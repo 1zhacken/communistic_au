ebab196647faf538891a48ddd2e429bf33afce0f
1zhacken <1zhacken@gmail.com>
2018-11-12 18:11:27 +0300

    init
    tu dovolna ka4ka?

---
+++ Assets/_scripts/camera_move.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_move : MonoBehaviour {
+public Transform target;
+public float smooth= 5.0f;
+public Vector3 offset = new Vector3(0, 0, -5);
+
+void Start()
+	{
+		transform.position = target.position + offset;
+	}
+void  Update (){
+    transform.position = Vector3.Lerp (transform.position, target.position + offset, Time.deltaTime * smooth);
+} 
+}
---
+++ Assets/_scripts/move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	public float speed = 5f;
+	public float speedRotation = 3f;
+	float curspeed;
+	private Rigidbody rb;
+	public Transform player;
+	public GameObject light;
+	Light myLight;
+	private float timeCount = 0f;
+
+	// Use this for initialization
+	void Start () {
+		curspeed = speed;
+		rb = GetComponent<Rigidbody>();
+		myLight = light.GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		if (Input.GetKey(KeyCode.W)) rb.AddForce(transform.right * speed * rb.mass);
+		if (Input.GetKey(KeyCode.S)) rb.AddForce(-transform.right * speed * rb.mass);
+		if (Input.GetKey(KeyCode.D)) transform.Rotate(Vector3.forward * speedRotation);
+		if (Input.GetKey(KeyCode.A)) transform.Rotate(Vector3.back * speedRotation);
+		if (Input.GetKeyDown(KeyCode.F)) myLight.enabled = !myLight.enabled;
+		if (Input.GetKey(KeyCode.Q))
+		{
+			rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(0, 0, 0), Time.deltaTime * speedRotation);
+			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(-90, 0, transform.rotation.z), Time.deltaTime * speedRotation);
+		}
+	}
+}
